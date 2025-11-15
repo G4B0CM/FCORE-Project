f8c3f01f238bcc90d327f846ec5136e90a209c17
Gabriel Calderón <130103971+G4B0CM@users.noreply.github.com>
2025-11-15 11:19:23 -0500

    Backend name changes

--- fe-fcore/next.config.ts
+++ fe-fcore/next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'primefaces.org',
+        pathname: '/cdn/primereact/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '4kwallpapers.com',
+        pathname: '/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.piranirisk.com',
+        pathname: '/hs-fs/hubfs/**',
+      },
+    ],
+  },
 };
-
 export default nextConfig;
